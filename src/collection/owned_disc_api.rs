96732cfc903f969aa89a0a381183dafa09d0e40e
Guillaume Lechelon <lechelon.guillaume@gmail.com>
2024-03-07 01:05:47 +0100

    FEAT: Ajout pico.css +refresh après ajout/suppression dans la collection

--- src/collection/owned_disc_api.rs
+++ src/collection/owned_disc_api.rs
@@ -6,7 +6,9 @@ use actix_web::{
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 
-use crate::{create_artist, create_disc, find_artist_id_by_name, is_artist_owned, is_disc_owned, OwnedDisc};
+use crate::{
+    create_artist, create_disc, find_artist_id_by_name, is_artist_owned, is_disc_owned, OwnedDisc,
+};
 
 pub(crate) fn configure_collecrtion_api(config: &mut web::ServiceConfig) {
     config.service(add_owned_disc).service(remove_owned_disc);
@@ -16,13 +18,13 @@ pub(crate) fn configure_collecrtion_api(config: &mut web::ServiceConfig) {
 async fn add_owned_disc(
     disc: web::Json<OwnedDisc>,
     db_connection_pool: Data<Pool<SqliteConnectionManager>>,
-) -> impl Responder {
+) -> HttpResponse {
     dbg!(&disc);
     let is_disc_already_present = is_disc_owned(&db_connection_pool, &disc.title);
     let is_artist_already_present = is_artist_owned(&db_connection_pool, &disc.artist.name);
 
     if is_disc_already_present {
-        return HttpResponse::Conflict();
+        return HttpResponse::Conflict().finish();
     }
 
     if !is_artist_already_present {
@@ -31,9 +33,10 @@ async fn add_owned_disc(
 
     let artist_id: i32 = find_artist_id_by_name(&db_connection_pool, &disc.artist.name);
     create_disc(&db_connection_pool, &disc, &artist_id);
-    
 
     HttpResponse::Created()
+        .insert_header(("HX-Trigger", "refreshSearch"))
+        .finish()
 }
 
 #[delete("api/owned-discs/{title}")]
@@ -50,4 +53,6 @@ async fn remove_owned_disc(
         .and_then(|mut statement| statement.execute([&title.to_string().replace("%20", " ")])) //TODO: La suppression par ID est préféreable
         .expect("Erreur lors de la création du disque");
     HttpResponse::Created()
+        .insert_header(("HX-Trigger", "refreshCollection"))
+        .finish()
 }
